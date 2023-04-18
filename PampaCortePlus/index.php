494ca6864ae2b66abbcba20b9df6295e315ada26
pedro <pedro.prccau@gmail.com>
2023-04-18 10:06:47 -0300

    efjkrqvpsqagrea

---
+++ PampaCortePlus/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Pampa Corte Plus</title>
+    </head>
+    <body>
+        <h2>Pampacorteplus</h2>
+ 
+            <table>
+                <tr>
+                    
+                    
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Nome</td>
+                    <td>
+                        <input type="text" name="nome" placeholder="Informe seu nome completo">
+                    </td>
+                    <td></td>
+                </tr>
+                
+                <tr>
+                    <td>E-mail</td>
+                    <td>
+                        <input type="text" name="telefone" placeholder="Informe seu email.">
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                   
+                <tr>
+                    <td>Telefone</td>
+                    <td>
+                        <input type="text" name="endereco" placeholder="Informe seu telefone">
+                    </td>
+                 
+                </tr>
+                <td>Atuação</td>
+                    <td>
+                        <textarea name="endereco" placeholder="Informe sua área de atuação"></textarea>
+                    </td>
+                
+                <tr>
+                    <td>Selecione um tipo de acesso</td>
+                    <td>
+                            <input type="radio" name="acesso" value="adm">Administrador
+                            <br>
+                            <input type="radio" name="acesso" value="estudante">Estudante
+                            <br>
+                            <input type="radio" name="acesso" value="produtor">Produtor
+                            <br>
+                            <input type="radio" name="acesso" value="professor">Professor 
+                            <br>
+                            <input type="radio" name="acesso" value="tecnico">Técnico
+                            <br>
+                            <input type="radio" name="acesso" value="visitante">Visitante                    
+                            </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="submit" value="Enviar">
+                        <input type="reset" value="Limpar campos">
+                    </td>
+                
+                    
+                    
+                    <td>6547W6478W64832742</td>
+              
+            </table>
+            <?php
+          
+            ?>
+ 
+    </body>
+</html>
