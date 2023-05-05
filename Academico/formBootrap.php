459dc6ca1c4da9d40a3cf7a44748d4d07e04f189
PEdro <pedro.prccau@gmail.com>
2023-05-05 10:01:32 -0300

    pussy

---
+++ Academico/formBootrap.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <title>Formulários</title>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+    </head>
+    <style>
+            .col-sm-12{
+                border: 1px solid red;
+            }
+    </style>
+    
+    <body>
+        <div class="container">
+            <form>
+                <div class="row page-header">
+                    <div class="col-sm-12">
+                        <h1>Menu: </h1>
+                    </div>
+                        
+                    
+                </div>
+                <div class="row">
+                    <div class="col-sm-8">
+
+                        <div class="form-group">
+                            <label for="usr">Name:</label>
+                             <input type="text" class="form-control" id="usr">
+                        </div>
+                        <div class="form-group">
+                            <label for="pwd">Password:</label>
+                            <input type="password" class="form-control" id="pwd">
+                        </div>
+                        
+                    
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        
+                    </div>
+                        
+                    
+                </div>
+            </form>
+            
+        </div>
+    </body>
+</html>
