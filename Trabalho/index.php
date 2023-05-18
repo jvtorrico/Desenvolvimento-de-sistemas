3a29f6f55823d4a4792c23b9480805abc874b665
Pedro <pedro.prccau@gmail.com>
2023-05-18 11:07:01 -0300

    Is dead

--- Trabalho/index.php
+++ Trabalho/index.php
@@ -4,28 +4,78 @@
     <head>
         <meta charset="UTF-8">
         <title>Trabalho de merda odeio DSI</title>
-        <link href="../Buttons/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script src="../Buttons/js/bootstrap.min.js" type="text/javascript"></script>
         <script src="../Buttons/js/jquery.min.js" type="text/javascript"></script>
     </head>
-    
-    <body>
-        <h2>Formulário de Ordem de Serviço - Bicho Chique Petshop</h2>
+
+    <body class="container">
         <form method="POST" action="index.php">
-            
-            <table >
-                <tr>
-                    <td>Nome do Cliente:<input type="text" value="nome" placeholder="Digite o nome do cliente" ></td>
-                   
-                </tr>
-            </table>
-            
-        </form>
-        
-        
-        
-        <?php
-        // put your code here
-        ?>
-    </body>
+            <div class="row page-header" >
+                <div class="col-sm-12">
+                    <h1>Formulário de Ordem de Serviço - Bicho Chique Petshop</h1>
+                </div>                
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <img src="img/Cachorro.PNG" alt="cachorro"/>
+                    </div>
+                </div>    
+                <div class="col-sm-8">
+                    <div class="form-group">
+                        <label for="nome">Nome do cliente: </label>
+                        <input type="text" class="form-control" id="nome" placeholder="Digite o nome do cliente" name="nome">
+                    </div>
+                    <div class="form-group">
+                        <label for="telefone">Telefone: </label>
+                        <input type="text" class="form-control" id="telefone" name="telefone" placeholder="Digite o número do cliente"> 
+                    </div>
+
+                    <div class="form-group">
+                        <label for="animal">Nome do animal: </label>
+                        <input type="text" class="form-control" id="animal" name="animal" placeholder="Digite o nome do animal"> 
+                    </div>
+                    <div class="form-group">
+                        <label>Serviços: </label>
+                        <div class="checkbox">
+                            <label><input type="checkbox" name="servico" value="Banho e tosa"><b>Banho e tosa</b></label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" name="servico" value="Corte de unhas"><b>Corte de unhas</b></label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" name="servico" value="Limpeza de ouvidos"><b>Limpeza de ouvidos</b></label>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" name="servico" value="Vacinação"><b>Vacinação</b></label>
+                        </div>
+                        <div class="form-group">
+                            <label for="data">Data: </label>
+                            <input type="date" class="form-control" name="data">
+                        </div>
+                        <div class="form-group">
+                            <label for="observacoes">Observações: </label>
+                            <textarea class="form-control" placeholder="Digite quaisquer observações"></textarea>
+                        </div>
+                        <input value="Enviar ordem de serviço"  class="btn btn-primary" type="submit">
+                    </div>
+
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+
+    </form>
+
+
+
+    <?php
+    // put your code here
+    ?>
+</body>
 </html>
